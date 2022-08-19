41d92398b58d8569623fa4a3ec5bfcfa51b93201
Igormcf <igormaciel82@gmail.com>
2022-08-19 15:21:00 -0300

    Altera Joi

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import LoginRoutes from './routes/login.routes';
 import 'express-async-errors';
+import validLogin from './middlewares/validLogin';
 
 class App {
   public app: express.Express;
@@ -25,6 +26,7 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
 
+    this.app.use(validLogin);
     this.app.use(LoginRoutes);
   }
 
